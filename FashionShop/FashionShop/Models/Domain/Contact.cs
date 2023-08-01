a6af4ea3455347c986e59dfa116cf27cb5666ba5
TSanzz1229 <119602416+TSanzz1229@users.noreply.github.com>
2023-08-01 08:49:25 +0700

    x

--- FashionShop/FashionShop/Models/Domain/Contact.cs
+++ FashionShop/FashionShop/Models/Domain/Contact.cs
@@ -10,7 +10,6 @@ namespace FashionShop.Models.Domain
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Content { get; set; }
-        public string FullName { get; set; }
         public bool Status { get; set; }
     }
 }
