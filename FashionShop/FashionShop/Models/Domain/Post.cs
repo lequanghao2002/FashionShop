f398e2d4d08b5e82726590ad4fe4c3f9fb44259e
Quang Hào <haolq2002@gmail.com>
2023-08-01 11:00:54 +0700

    Tạo database

--- FashionShop/FashionShop/Models/Domain/Post.cs
+++ FashionShop/FashionShop/Models/Domain/Post.cs
@@ -1,10 +1,22 @@
-﻿namespace FashionShop.Models.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FashionShop.Models.Domain
 {
+    [Table("Posts")]
     public class Post
     {
-        public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Image { get; set; }
+
+        [Required]
         public string Content { get; set; }
         public bool Status { get; set; }
     }
