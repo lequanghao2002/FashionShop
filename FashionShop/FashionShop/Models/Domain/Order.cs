e1f29ebed7d412b5b5937a5e3c366df32799409b
thanhsu <tsu14758@gmail.com>
2023-08-01 08:07:02 +0700

    tao class domain products

--- FashionShop/FashionShop/Models/Domain/Order.cs
+++ FashionShop/FashionShop/Models/Domain/Order.cs
@@ -1,6 +1,24 @@
-﻿namespace FashionShop.Models.Domain
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FashionShop.Models.Domain
 {
-    public class Order
+    public class Products
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CatetoryID { get; set; }
+        public int Quantity { get; set; }
+        public string Describe { get; set; }
+        public string Image { get; set; }
+
+        [Column(TypeName = "xml")]
+        public string ListImage { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedBy { get; set;}
+        public DateTime UpdatedDate { get; set;}
+        public DateTime UpdatedBy { get; set;}
+        public bool Status { get; set; }
     }
 }
