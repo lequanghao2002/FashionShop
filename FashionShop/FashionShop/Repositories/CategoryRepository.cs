0f2a38335410e736c6c241daa0150ecd92444b7c
thanhsu <tsu14758@gmail.com>
2023-08-03 14:08:00 +0700

    làm CategoryRepository, DTO, Controller

--- FashionShop/FashionShop/Repositories/CategoryRepository.cs
+++ FashionShop/FashionShop/Repositories/CategoryRepository.cs
@@ -1,6 +1,74 @@
-﻿namespace FashionShop.Repositories
+﻿using FashionShop.Data;
+using FashionShop.Models.Domain;
+using FashionShop.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FashionShop.Repositories
 {
-    public class CategoryRepository
+    public interface ICategoryRepository
     {
+        List<CategoryDTO> GetAllCategory();
+        CategoryByIdDTO GetCategoryById(int id);
+        AddCategoryRequestDTO AddCategory(AddCategoryRequestDTO addCategoryRequestDTO);
+        AddCategoryRequestDTO? UpdateCategoryById(int id, AddCategoryRequestDTO CategoryDTO);
+        DeleteCategoryDTO DeleteCategoryById(int id, DeleteCategoryDTO categoryDomain);
+    }
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly FashionShopDBContext _IdentityDbContext;
+        public CategoryRepository(FashionShopDBContext IdentityDbContext)
+        {
+            _IdentityDbContext = IdentityDbContext;
+        }
+        public List<CategoryDTO> GetAllCategory()
+        {
+            var allCategory = _IdentityDbContext.Categories.Select(Categorys => new CategoryDTO()
+            {
+                ID = Categorys.ID,
+                Name = Categorys.Name
+            }).ToList();
+            return allCategory;
+        }
+        public CategoryByIdDTO GetCategoryById(int id)
+        {
+            var CategoryWithDomain = _IdentityDbContext.Categories.Where(n => n.ID == id);
+            var CategoryWithIdDTO = CategoryWithDomain.Select(Categorys => new CategoryByIdDTO()
+            {
+                Name = Categorys.Name
+            }).FirstOrDefault();
+            return CategoryWithIdDTO;
+        }
+        public AddCategoryRequestDTO AddCategory(AddCategoryRequestDTO addCategoryRequestDTO)
+        {
+            var CategoryDomainModel = new Category
+            {
+                Name = addCategoryRequestDTO.Name
+            };
+            _IdentityDbContext.Categories.Add(CategoryDomainModel);
+            _IdentityDbContext.SaveChanges();
+            return addCategoryRequestDTO;
+        }
+        public AddCategoryRequestDTO? UpdateCategoryById(int id, AddCategoryRequestDTO CategoryDTO)
+        {
+            var CategoryDomain = _IdentityDbContext.Categories.FirstOrDefault(n => n.ID == id);
+            if (CategoryDomain != null)
+            {
+                CategoryDomain.Name = CategoryDTO.Name;
+                _IdentityDbContext.SaveChanges();
+            }
+            return CategoryDTO;
+        }
+        public DeleteCategoryDTO DeleteCategoryById(int id, DeleteCategoryDTO categoryDomain)
+        {
+            var CategoryDomain = _IdentityDbContext.Categories.FirstOrDefault(n => n.ID == id);
+            if(CategoryDomain != null)
+            {
+                _IdentityDbContext.Categories.Remove(CategoryDomain);
+                _IdentityDbContext.SaveChanges();
+            }
+            return categoryDomain;
+        }
     }
 }
