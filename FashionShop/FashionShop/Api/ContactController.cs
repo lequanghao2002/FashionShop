703786afd51e0982110711dffc0113518ccc0b69
TSanzz1229 <119602416+TSanzz1229@users.noreply.github.com>
2023-08-05 22:32:17 +0700

    Contact API

---
+++ FashionShop/FashionShop/Api/ContactController.cs
@@ -0,0 +1,58 @@
+﻿using FashionShop.Data;
+using FashionShop.Models.Domain;
+using FashionShop.Models.DTO;
+using FashionShop.Repositories;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionShop.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly FashionShopDBContext _identityDbContext;
+        private readonly IContactRepository _icontactRepository;
+        public ContactController(FashionShopDBContext identityDbContext, IContactRepository icontactRepository)
+        {
+            _identityDbContext = identityDbContext;
+            _icontactRepository = icontactRepository;
+        }
+
+        [HttpGet("get-all-contact")]
+        public IActionResult GetAll()
+        {
+            var allcontacts = _icontactRepository.GetAllContact();
+            return Ok(allcontacts);
+        }
+
+        [HttpPut("update-contact-by-id/{id}")]
+        public IActionResult UpdateContactById(int id, [FromBody] UpdateContactDTO updatecontactDTO)
+        {
+            var updatContact = _icontactRepository.UpdateContactById(id, updatecontactDTO);
+            return Ok(updatContact);
+        }
+
+        [HttpPost("add-contact")]
+        public IActionResult AddContact([FromBody] AddContactDTO addContactDTO)
+        {
+            var addcontact = _icontactRepository.AddContact(addContactDTO);
+            return Ok(addcontact);
+        }
+
+        [HttpDelete("delete-category-by-id/{id}")]
+        public IActionResult DeleteContact(int id)
+        {
+            var deletecontact = _icontactRepository.DeleteContact(id);
+            return Ok(deletecontact);
+        }
+
+        [HttpGet("find-by-phonenumber/{phonenumber}")]
+        public IActionResult FindContact(string Phonenumber)
+        {
+            var FindUser = _icontactRepository.FindContact(Phonenumber);
+            return Ok(FindUser);
+        }
+    }
+}
--- FashionShop/FashionShop/Repositories/ContactRepository.cs
+++ FashionShop/FashionShop/Repositories/ContactRepository.cs
@@ -1,6 +1,94 @@
-﻿namespace FashionShop.Repositories
+﻿using FashionShop.Models.DTO;
+using FashionShop.Models.Domain;
+using FashionShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FashionShop.Repositories
 {
-    public class ContactRepository
+    public interface IContactRepository
     {
+        List<ContactDTO> GetAllContact();
+        AddContactDTO AddContact(AddContactDTO addContactDTO);
+        UpdateContactDTO UpdateContactById(int id, UpdateContactDTO updatecontactDTO);
+        Contact DeleteContact(int id);
+        FindByPhoneDTO FindContact(string Phonenumber);
+    }
+    public class ContactRepository : IContactRepository
+    {
+        private readonly FashionShopDBContext _identityDbContext;
+        public ContactRepository(FashionShopDBContext identityDbContext)
+        {
+            _identityDbContext = identityDbContext;
+        }
+        public List<ContactDTO> GetAllContact()
+        {
+            var allContact = _identityDbContext.Contacts.ToList();
+            var allContactDTO = new List<ContactDTO>();
+            foreach (var contact in allContact)
+            {
+                allContactDTO.Add(new ContactDTO()
+                {
+                    ID = contact.ID,
+                    FullName = contact.FullName,
+                    Email = contact.Email,
+                    PhoneNumber = contact.PhoneNumber,
+                });
+            }
+            return allContactDTO;
+        }
+        public AddContactDTO AddContact(AddContactDTO addContactDTO)
+        {
+            var ContactDM = new Contact
+            {
+                FullName = addContactDTO.FullName,
+                Email = addContactDTO.Email,
+                PhoneNumber = addContactDTO.PhoneNumber,
+                Content= addContactDTO.Content,
+                Status= addContactDTO.Status,
+            };
+            _identityDbContext.Contacts.Add(ContactDM);
+            _identityDbContext.SaveChanges();
+            return addContactDTO;
+        }
+        public UpdateContactDTO UpdateContactById(int id, UpdateContactDTO updatecontactDTO)
+        {
+            var ContactDm = _identityDbContext.Contacts.FirstOrDefault(n => n.ID == id);
+            if (ContactDm != null)
+            {
+                ContactDm.FullName = updatecontactDTO.FullName;
+                ContactDm.PhoneNumber = updatecontactDTO.PhoneNumber;
+                ContactDm.Email = updatecontactDTO.Email;
+                _identityDbContext.SaveChanges();
+            }
+            return updatecontactDTO;
+        }
+        public Contact DeleteContact(int id)
+        {
+            var DeletedM = _identityDbContext.Contacts.FirstOrDefault(n => n.ID == id);
+            if (DeletedM != null)
+            {
+                _identityDbContext.Contacts.Remove(DeletedM);
+                _identityDbContext.SaveChanges();
+            }
+            return DeletedM;
+        }
+        public FindByPhoneDTO FindContact(string Phonenumber)
+        {
+            var findcontacWDM = _identityDbContext.Contacts.FirstOrDefault(x => x.PhoneNumber == Phonenumber);
+            if (findcontacWDM == null)
+            {
+                return null;
+            }
+            var findbyphoneDTO = new FindByPhoneDTO
+            {
+                ID = findcontacWDM.ID,
+                FullName = findcontacWDM.FullName,
+                Email = findcontacWDM.Email,
+            };
+            return findbyphoneDTO;
+        }
+    
     }
 }
