5200d3f969a465bf0b465655a004cd211408455b
Quang Hào <haolq2002@gmail.com>
2023-08-13 22:54:33 +0700

    Nhúng template khách hàng

---
+++ FashionShop/FashionShop/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FashionShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
